7f7329e9a8932677dfc65ac482b89f674fa2ca2a
kevin.orrego <kevin.orrego@pragma.com.co>
2021-09-05 20:45:38 -0500

    first entity created

---
+++ employees.Common/Models/Entry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace employees.Common.Models
+{
+    public class Entry
+    {
+        public int EmployeeId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public DateTime Hour { get; set; }
+
+        public int Type { get; set; }
+
+        public bool IsConsolidated { get; set; }
+    }
+}
---
+++ employees.Functions/Entities/EntryEntity.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace employees.Functions.Entities
+{
+    public class EntryEntity : TableEntity
+    {
+        public int EmployeeId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public DateTime Hour { get; set; }
+
+        public int Type { get; set; }
+
+        public bool IsConsolidated { get; set; }
+    }
+}
